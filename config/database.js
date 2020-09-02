9e1278f28ff623b98293493a5f79358df519877c
Leydi Camargo <leydi.camargo@gmail.com>
2020-09-02 16:01:47 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -9,9 +9,9 @@ module.exports = {
   },
 
   hrPool: {
-    user: process.env.ORA_USER_MONITOR = "USURIPSSDF",
-    password: process.env.ORA_PASSWORD_MONITOR = "Desaripss2019",
-    connectString: process.env.ORA_CONNECTIONSTRING_MONITOR = "srvvdesabd-scan.colsanitas.com:1521/DRIPSSDF",
+    user: process.env.ORA_USER_MONITOR = "RIPSS",
+    password: process.env.ORA_PASSWORD_MONITOR = "yE91ijGeTL",
+    connectString: process.env.ORA_CONNECTIONSTRING_MONITOR = "204.110.52.42:1521/ORCL",
     poolMin: 10,
     poolMax: 10,
     poolIncrement: 0
